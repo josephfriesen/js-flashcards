0a68a47dc210d763c198acd0647b12640d31c310
Evan Filkins and Joseph Friesen <evan@>
2018-08-07 14:40:11 -0700

    Enable cards to toggle between terms and definitions, provide content to definitions.

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,166 @@ $(document).ready(function() {
     $("#javascript-title").fadeToggle();
     $("#javascript-def").fadeToggle();
   });
+
+  $("#operators-title").click(function() {
+    $("#operators-title").fadeToggle();
+    $("#operators-def").fadeToggle();
+  });
+  $("#operators-def").click(function() {
+    $("#operators-title").fadeToggle();
+    $("#operators-def").fadeToggle();
+  });
+
+  $("#variables-title").click(function() {
+    $("#variables-title").fadeToggle();
+    $("#variables-def").fadeToggle();
+  });
+  $("#variables-def").click(function() {
+    $("#variables-title").fadeToggle();
+    $("#variables-def").fadeToggle();
+  });
+
+  $("#naming-title").click(function() {
+    $("#naming-title").fadeToggle();
+    $("#naming-def").fadeToggle();
+  });
+  $("#naming-def").click(function() {
+    $("#naming-title").fadeToggle();
+    $("#naming-def").fadeToggle();
+  });
+
+  $("#functions-title").click(function() {
+    $("#functions-title").fadeToggle();
+    $("#functions-def").fadeToggle();
+  });
+  $("#functions-def").click(function() {
+    $("#functions-title").fadeToggle();
+    $("#functions-def").fadeToggle();
+  });
+
+  $("#methods-title").click(function() {
+    $("#methods-title").fadeToggle();
+    $("#methods-def").fadeToggle();
+  });
+  $("#methods-def").click(function() {
+    $("#methods-title").fadeToggle();
+    $("#methods-def").fadeToggle();
+  });
+
+  $("#arguments-title").click(function() {
+    $("#arguments-title").fadeToggle();
+    $("#arguments-def").fadeToggle();
+  });
+  $("#arguments-def").click(function() {
+    $("#arguments-title").fadeToggle();
+    $("#arguments-def").fadeToggle();
+  });
+
+  $("#parameters-title").click(function() {
+    $("#parameters-title").fadeToggle();
+    $("#parameters-def").fadeToggle();
+  });
+  $("#parameters-def").click(function() {
+    $("#parameters-title").fadeToggle();
+    $("#parameters-def").fadeToggle();
+  });
+
+  $("#return-title").click(function() {
+    $("#return-title").fadeToggle();
+    $("#return-def").fadeToggle();
+  });
+  $("#return-def").click(function() {
+    $("#return-title").fadeToggle();
+    $("#return-def").fadeToggle();
+  });
+
+  $("#chaining-title").click(function() {
+    $("#chaining-title").fadeToggle();
+    $("#chaining-def").fadeToggle();
+  });
+  $("#chaining-def").click(function() {
+    $("#chaining-title").fadeToggle();
+    $("#chaining-def").fadeToggle();
+  });
+
+  $("#strings-title").click(function() {
+    $("#strings-title").fadeToggle();
+    $("#strings-def").fadeToggle();
+  });
+  $("#strings-def").click(function() {
+    $("#strings-title").fadeToggle();
+    $("#strings-def").fadeToggle();
+  });
+
+  $("#booleans-title").click(function() {
+    $("#booleans-title").fadeToggle();
+    $("#booleans-def").fadeToggle();
+  });
+  $("#booleans-def").click(function() {
+    $("#booleans-title").fadeToggle();
+    $("#booleans-def").fadeToggle();
+  });
+
+  $("#undefined-title").click(function() {
+    $("#undefined-title").fadeToggle();
+    $("#undefined-def").fadeToggle();
+  });
+  $("#undefined-def").click(function() {
+    $("#undefined-title").fadeToggle();
+    $("#undefined-def").fadeToggle();
+  });
+
+  $("#nan-title").click(function() {
+    $("#nan-title").fadeToggle();
+    $("#nan-def").fadeToggle();
+  });
+  $("#nan-def").click(function() {
+    $("#nan-title").fadeToggle();
+    $("#nan-def").fadeToggle();
+  });
+
+  $("#escape-title").click(function() {
+    $("#escape-title").fadeToggle();
+    $("#escape-def").fadeToggle();
+  });
+  $("#escape-def").click(function() {
+    $("#escape-title").fadeToggle();
+    $("#escape-def").fadeToggle();
+  });
+
+  $("#alert-title").click(function() {
+    $("#alert-title").fadeToggle();
+    $("#alert-def").fadeToggle();
+  });
+  $("#alert-def").click(function() {
+    $("#alert-title").fadeToggle();
+    $("#alert-def").fadeToggle();
+  });
+
+  $("#comments-title").click(function() {
+    $("#comments-title").fadeToggle();
+    $("#comments-def").fadeToggle();
+  });
+  $("#comments-def").click(function() {
+    $("#comments-title").fadeToggle();
+    $("#comments-def").fadeToggle();
+  });
+
+  $("#jquery-title").click(function() {
+    $("#jquery-title").fadeToggle();
+    $("#jquery-def").fadeToggle();
+  });
+  $("#jquery-def").click(function() {
+    $("#jquery-title").fadeToggle();
+    $("#jquery-def").fadeToggle();
+  });
+
+  $("#attributes-title").click(function() {
+    $("#attributes-title").fadeToggle();
+    $("#attributes-def").fadeToggle();
+  });
+  $("#attributes-def").click(function() {
+    $("#attributes-title").fadeToggle();
+    $("#attributes-def").fadeToggle();
+  });
 });
